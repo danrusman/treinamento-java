96406f4802dd2549f2deb1aec2cdce2448ac0132
danrusman <darman0551@gmail.com>
2021-09-30 20:32:57 -0300

    URI problem 1059 solved

---
+++ uri/Uri/Uri1059.java
@@ -0,0 +1,9 @@
+package Uri;
+
+public class Uri1059 {
+    public static void main(String[] args) {
+        for (int numeroPar = 2; numeroPar <= 100; numeroPar+= 2) {
+            System.out.println(numeroPar);
+        }
+    }
+}
