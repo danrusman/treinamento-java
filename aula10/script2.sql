42f398fe654008f301bea28cc66b744bcec9dda8
danrusman <darman0551@gmail.com>
2021-10-13 15:57:54 -0300

    Atualixacao

---
+++ aula10/script2.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bdspring02;
+
+USE bdspring02;
+
+INSERT INTO tb_produto VALUES (
+	null, "mouse gamer", 200
+);
+
+INSERT INTO tb_produto VALUES (
+	null, "teclado game", 300
+);
+
+SELECT * FROM tb_produto;
